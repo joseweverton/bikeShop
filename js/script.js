a6572a22695232fd5dcec54ba5765f3dc92a2a5f
jwcosta-br <jwcosta.br@gmail.com>
2023-07-15 14:32:16 -0300

    URL parameters js.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Active link header-menu
 const links = document.querySelectorAll(".header-menu a")
 
 function activeLink(link) {
@@ -8,5 +9,16 @@ function activeLink(link) {
         link.classList.add("active")
     }
 }
-
 links.forEach(activeLink)
+
+//Active link insurance items
+const parameters = new URLSearchParams(location.search)
+
+function activeProduct(parameter) {
+    const element = document.getElementById(parameter)
+    if (element) {
+    element.checked = true
+    }
+}
+
+parameters.forEach(activeProduct)
