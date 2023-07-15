3a80945ac2b031f422b70a15832d2a6df28e8bdd
jwcosta-br <jwcosta.br@gmail.com>
2023-07-15 15:51:16 -0300

    Implemented expand event with JavaScript in Frequently Asked Questions.

--- js/script.js
+++ js/script.js
@@ -20,5 +20,25 @@ function activeProduct(parameter) {
     element.checked = true
     }
 }
-
 parameters.forEach(activeProduct)
+
+//Frequently Asked Questions
+const questions = document.querySelectorAll(".questions button")
+
+function activeQuestion(event) {
+    const question = event.currentTarget
+    const controls = question.getAttribute("aria-controls")
+    const answer = document.getElementById(controls)
+    
+    answer.classList.toggle("active")
+    const active = answer.classList.contains("active")
+    question.setAttribute("aria-expanded", active)
+    
+}
+
+function eventQuestions(ask) {
+    ask.addEventListener("click", activeQuestion)
+}
+
+questions.forEach(eventQuestions)
+
